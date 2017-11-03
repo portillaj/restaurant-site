2ac71eb47edfb33da705065c708bcd5d35826e6e
Jason Portilla <jasonportilla@Jasons-MacBook-Pro.local>
2017-11-03 14:48:11 -0400

    fixed error

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,8 @@ gulp.task('js', function(){
 //Watch Sass & Server
 gulp.task('serve', ['sass'], function(){
   browserSync.init({
-    server: "./src"
+    server: "./src",
+    open: false
   });
 
   gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
