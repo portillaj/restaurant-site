7413f77a8da4cda382732cc81695f268d59729d9
Jason Portilla <jasonportilla@Jasons-MacBook-Pro.local>
2017-11-03 10:32:39 -0400

    fixed error

--- gulpfile.js
+++ gulpfile.js
@@ -41,3 +41,11 @@ gulp.task('fa', function(){
 });
 
 gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
+
+gulp.task('serveprod', function() {
+  connect.server({
+    root: index.html,
+    port: process.env.PORT || 5000, // localhost:5000
+    livereload: false
+  });
+});
